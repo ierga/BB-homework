e74cd50d4ac9534343376e0c01daea4cf6daa38e
Ilkhom <iergashbaev@gmail.com>
2018-04-29 15:48:11 +0200

    functions tests from section two are passed

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,57 +1,54 @@
 // Variables
 
 var hero = {
-  name: 'Predator',
+  name: 'Django',
   heroic: true,
   inventory: [],
-  health: 10,
-  weapon: {type: 'gun', damage: 5}
+  health: 100,
+  weapon: {type: 'Revolver', damage: 10}
 }
 
-var creature = {
-  name: 'Alien',
-  heroic: false,
-  inventory: [],
-  health: 10,
-  item: {type: 'gun', damage: 5}
-}
-
-var attacker = {
-  name: 'Predator',
-  heroic: true,
-  inventory: [],
-  health: 10,
-  weapon: {type: 'gun', damage: 5}
-}
-
-var defender = {
-  name: 'Predator',
-  heroic: true,
-  inventory: [],
-  health: 10,
-  weapon: {type: 'gun', damage: 5}
-}
 // Game logic
 
 function rest(creature) {
-  creature.health = 10
-  return creature
+  creature.health = 10;
+  return creature;
 }
 
 function pickUpItem(creature,item) {
-
+  creature.inventory.push(item);
+  return creature;
 }
 
 function dealDamage(attacker,defender) {
-
+  defender.health = defender.health - attacker.weapon.damage;
+  return defender;
 }
 
-function equipWeapon() {
-
-
+function equipWeapon(creature,index) {
+  creature.weapon = creature.inventory[index];
+  creature.inventory.shift(index);
+  return creature;
 }
 
-function doBattle() {
+function doBattle(heroicCreature, creature) {
+  if(heroicCreature.heroic !== true) {
+    return null;
+  }
 
+  while(heroicCreature.health > 0 && creature.health > 0) {
+    dealDamage(heroicCreature, creature);
+
+    if(creature.health > 0) {
+      dealDamage(creature, heroicCreature);
+    }
+  }
+
+  if(heroicCreature.health > 0) {
+      return heroicCreature;
+  } else {
+      alert("Your hero is dead!");
+  }
 }
+
 // UI
